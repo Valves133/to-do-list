2bd21100709f3207d3073ab6267a8b0fd59c505e
Victor Alves <victor.h.m.alves@gmail.com>
2020-05-06 18:26:03 +0200

    creation class controller and routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 
+import TaskController from './controllers/TaskController';
+import './models/task';
+
 const routes = Router();
 
-routes.get('/todo', (req,res) => res.json({message:"TodoList"}));
+routes.post('/task', TaskController.store);
+routes.get('/task', TaskController.show);
+routes.get('/task/:id',checkIdExist, TaskController.index);
+routes.put('/task/:id',checkIdExist, TaskController.update);
+routes.delete('/task/:id',checkIdExist, TaskController.delete);
 
 export default routes;
